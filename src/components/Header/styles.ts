541c997ecfc71a023d96db651a593ced3511b3f2
Bruno Mateus <bruno_205@hotmail.com>
2022-10-18 05:20:44 -0300

    add header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import cover from '../../assets/Cover.png'
+
+export const HeaderContent = styled.header`
+  background-image: url(${cover});
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 18.5rem;
+`
